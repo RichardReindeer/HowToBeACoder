10e59f32a62d64d6da02ae23a6416a47dbbc9f58
RichardReindeer <510614397@qq.com>
2021-04-25 13:12:59 +0800

    4.25

---
+++ JVMstaff/src/com/bambi/enumLearn/EnumTest.java
@@ -0,0 +1,8 @@
+package com.bambi.enumLearn;
+
+public class EnumTest {
+    public static void main(String[] args) {
+        //直接引用
+        Day day = Day.MONDAY;
+    }
+}
---
+++ JVMstaff/src/com/bambi/singleTon/SingleTon07Test.java
@@ -0,0 +1,10 @@
+package com.bambi.singleTon;
+
+/**
+ * 调用枚举类创建的实例
+ */
+public class SingleTon07Test {
+    public static void main(String[] args) {
+        SingleTon07.INSTANCE.test();
+    }
+}
