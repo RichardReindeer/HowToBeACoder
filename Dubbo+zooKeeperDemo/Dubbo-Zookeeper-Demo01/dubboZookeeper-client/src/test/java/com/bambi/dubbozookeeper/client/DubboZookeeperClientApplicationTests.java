626616d54c44bdbda1956e86c8f6f350bd8a3f87
RichardReindeer <44000760+RichardReindeer@users.noreply.github.com>
2021-05-23 21:35:01 +0800

    2021/05/23

---
+++ Dubbo+zooKeeperDemo/Dubbo-Zookeeper-Demo01/dubboZookeeper-client/src/test/java/com/bambi/dubbozookeeper/client/DubboZookeeperClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bambi.dubbozookeeper.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DubboZookeeperClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
