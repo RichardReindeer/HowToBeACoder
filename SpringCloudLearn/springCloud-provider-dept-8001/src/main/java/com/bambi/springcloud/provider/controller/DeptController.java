43f70182f6009a54fc2e2582e740b825dcf88a39
RichardReindeer <44000760+RichardReindeer@users.noreply.github.com>
2021-05-26 18:53:04 +0800

    2021/05/26

---
+++ SpringCloudLearn/springCloud-provider-dept-8001/src/main/java/com/bambi/springcloud/provider/controller/DeptController.java
@@ -0,0 +1,37 @@
+package com.bambi.springcloud.provider.controller;
+
+import com.bambi.springcloud.api.vo.Dept;
+import com.bambi.springcloud.provider.service.IDeptService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * 提供Restful服务
+ */
+@RestController
+public class DeptController {
+
+    @Resource
+    IDeptService service;
+
+    @PostMapping("/dept/add")
+    public boolean addDept(Dept dept){
+        return service.addDept(dept);
+    }
+
+    @GetMapping("/dept/get/{id}")
+    public Dept get(@PathVariable("id") Long id){
+        return service.queryDeptById(id);
+    }
+
+    @PostMapping("/dept/list")
+    public List<Dept> queryAll(){
+        return service.queryAll();
+    }
+}
