485847a7545227df71f317720121db0c3b12371b
RichardReindeer <44000760+RichardReindeer@users.noreply.github.com>
2021-05-27 16:34:18 +0800

    2021/05/27

---
+++ SpringCloudLearn/springCloud-eureka-7001/src/main/java/ocm/bambi/springcloud/EurekaServer_7001.java
@@ -0,0 +1,15 @@
+package ocm.bambi.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+/*Eureka服务开启 服务端启动类，接受其他服务注册进来*/
+@EnableEurekaServer
+public class EurekaServer_7001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7001.class);
+    }
+}
