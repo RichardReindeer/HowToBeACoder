3fb0770811fb5947c1c12c77db9398dccbf01385
RichardReindeer <44000760+RichardReindeer@users.noreply.github.com>
2021-05-28 09:20:29 +0800

    2021/05/27

--- SpringCloudLearn/springCloud-provider-dept-8001/src/main/java/com/bambi/springcloud/provider/DeptProvider_8001.java
+++ SpringCloudLearn/springCloud-provider-dept-8001/src/main/java/com/bambi/springcloud/provider/DeptProvider_8001.java
@@ -3,10 +3,14 @@ package com.bambi.springcloud.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //启动类
 @SpringBootApplication
-
+@EnableEurekaClient  //在服务启动后自动注册到Eureka中
+//服务发现
+@EnableDiscoveryClient
 public class DeptProvider_8001 {
 
     public static void main(String[] args) {
--- SpringCloudLearn/springCloud-provider-dept-8001/src/main/java/com/bambi/springcloud/provider/controller/DeptController.java
+++ SpringCloudLearn/springCloud-provider-dept-8001/src/main/java/com/bambi/springcloud/provider/controller/DeptController.java
@@ -2,6 +2,8 @@ package com.bambi.springcloud.provider.controller;
 
 import com.bambi.springcloud.api.vo.Dept;
 import com.bambi.springcloud.provider.service.IDeptService;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -16,6 +18,10 @@ public class DeptController {
     @Resource
     IDeptService service;
 
+    //获取这些类的信息,得到具体的微服务
+    @Resource
+    private DiscoveryClient client;
+
     @PostMapping("/dept/add")
     public boolean addDept( @RequestBody Dept dept){
         return service.addDept(dept);
@@ -29,5 +35,19 @@ public class DeptController {
     @GetMapping("/dept/list")
     public List<Dept> queryAll(){
         return service.queryAll();
+    }
+
+    @GetMapping("/dept/discovery")
+    //注册进来的微服务，获取一些信息
+    public Object discovery(){
+        //获得微服务列表的清单
+        List<String> services = client.getServices();
+        services.forEach(s -> System.out.println(s));
+        //得到一个具体的微服务信息
+        //需要传入服务实例id applicationName
+        List<ServiceInstance> instances = client.getInstances("SPRINGCLOUD-PROVIDER-DEPT-8001");
+        instances.forEach(serviceInstance -> System.out.println("serviceInstance's host = " + serviceInstance.getHost()));
+
+        return this.client;
     }
 }
