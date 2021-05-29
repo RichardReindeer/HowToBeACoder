77468c83f3bd9fa725cf4ababdf3fa7d1dac5ac3
RichardReindeer <44000760+RichardReindeer@users.noreply.github.com>
2021-05-29 15:16:43 +0800

    2021/05/29

---
+++ SpringCloudTrain02/springcloud-demo01-api/src/main/java/com/bambi/springcloud/demo01/api/SpringcloudDemo01ApiApplication.java
@@ -0,0 +1,13 @@
+package com.bambi.springcloud.demo01.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudDemo01ApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudDemo01ApiApplication.class, args);
+    }
+
+}
