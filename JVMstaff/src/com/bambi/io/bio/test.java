01893dddcd1e75d1d29273f01f8c72c388f55f89
RichardReindeer <510614397@qq.com>
2021-06-20 16:27:14 +0800

    2021/06/20

---
+++ JVMstaff/src/com/bambi/io/bio/test.java
@@ -0,0 +1,13 @@
+package com.bambi.io.bio;
+
+public class test {
+    public static void main(String[] args) {
+        int b = 35;
+        double c = 35;
+        for(int i =0;i<10;i++){
+            c= (c*0.8);
+            b+= c;
+        }
+        System.out.println(b);
+    }
+}
